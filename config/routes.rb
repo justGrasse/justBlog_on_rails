b8586b798d6e118c6e1b456e251804972dceec7b
Justin G <email": null>
2017-01-22 14:25:44 -0800

    add RESTful route for users

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,16 @@ Rails.application.routes.draw do
   put 'blog_posts/:id' => 'blog_posts#update'
   delete 'blog_posts/:id' => 'blog_posts#destroy'
 
+  # users routes [  alt:  resources :users  ]
+  get 'users/' => 'users#index'
+  get 'users/new' => 'users#new',
+        as: 'new_user'
+  post 'users' => 'users#create'
+  get 'users/:id' => 'users#show',
+        as: 'user'
+  get 'users/:id/edit' => 'users#edit',
+        as: 'edit_user'
+  put 'users/:id' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
+
 end
