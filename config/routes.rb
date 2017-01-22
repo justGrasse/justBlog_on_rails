b04ac157e02714b33fb6f0d3c16a2997c7931630
Justin G <email": null>
2017-01-22 14:24:29 -0800

    add RESTful route for blog_posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'static/info'
-
-  get 'info' => 'static#info'
+  # blog_posts routes [  alt:  resources :blog_posts  ]
+  get 'blog_posts/' => 'blog_posts#index'
+  get 'blog_posts/new' => 'blog_posts#new',
+        as: 'new_blog_post'
+  post 'blog_posts' => 'blog_posts#create'
+  get 'blog_posts/:id' => 'blog_posts#show',
+        as: 'blog_post'
+  get 'blog_posts/:id/edit' => 'blog_posts#edit',
+        as: 'edit_blog_post'
+  put 'blog_posts/:id' => 'blog_posts#update'
+  delete 'blog_posts/:id' => 'blog_posts#destroy'
 
 end
